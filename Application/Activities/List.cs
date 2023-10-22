e20b6cea8939ac0577d20118686bbe189bfcc79c
Jason <devop.jas@gmail.com>
2023-10-23 00:36:25 +0800

    Added cancellation token

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -3,28 +3,29 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
-
 namespace Application.Activities
 {
     public class List
     {
-        public class Query: IRequest<List<Activity>>{ //query/request list of activitis
-
+        public class Query
+            : IRequest<
+                List<Activity>
+            > { //query/request list of activitis
         }
 
-        
-        public class Handler : IRequestHandler<Query, List<Activity>> //will handle the query 
+        public class Handler : IRequestHandler<Query, List<Activity>> //will handle the query
         {
             private readonly DataContext _context;
+
             public Handler(DataContext context)
             {
                 _context = context;
             }
-            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+
+            public async Task<List<Activity>> Handle(Query request, CancellationToken token)
             {
                 return await _context.Activities.ToListAsync();
             }
-
         }
     }
 }
